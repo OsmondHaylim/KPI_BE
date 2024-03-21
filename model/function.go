d39e97b6a39958f64dce00341c102afda1251857
OsmondHaylim <osmond20001@mail.unpad.ac.id>
2024-03-21 15:16:47 +0700

    anticipation for /0

--- model/function.go
+++ model/function.go
@@ -11,23 +11,37 @@ func (p Factor) ToPercentage() [][]Monthly{
 		for _, act := range p.Actual.Monthly{
 			for _, data := range p.Plan.Monthly{
 				b := "Percentage for Plan MonthlyID " + strconv.Itoa(data.Monthly_ID) + " actual MonthlyID " + strconv.Itoa(act.Monthly_ID) + " in Factor ID " + strconv.Itoa(p.Factor_ID) 
-				// if p.Target == "Zero"{
-					
-				// }
-				a := Monthly{
-					Jan: (data.Jan / act.Jan) * 100,
-					Feb: (data.Feb / act.Feb) * 100,
-					Mar: (data.Mar / act.Mar) * 100,
-					Apr: (data.Apr / act.Apr) * 100,
-					May: (data.May / act.May) * 100,
-					Jun: (data.Jun / act.Jun) * 100,
-					Jul: (data.Jul / act.Jul) * 100,
-					Aug: (data.Aug / act.Aug) * 100,
-					Sep: (data.Sep / act.Sep) * 100,
-					Oct: (data.Oct / act.Oct) * 100,
-					Nov: (data.Nov / act.Nov) * 100,
-					Dec: (data.Dec / act.Dec) * 100,
-					Remarks: &b,
+				var a Monthly
+				if p.Target == "Zero"{
+					a = Monthly{
+						Jan: ((data.Jan + 1) / (act.Jan + 1)) * 100,
+						Feb: ((data.Feb + 1) / (act.Feb + 1)) * 100,
+						Mar: ((data.Mar + 1) / (act.Mar + 1)) * 100,
+						Apr: ((data.Apr + 1) / (act.Apr + 1)) * 100,
+						May: ((data.May + 1) / (act.May + 1)) * 100,
+						Jun: ((data.Jun + 1) / (act.Jun + 1)) * 100,
+						Jul: ((data.Jul + 1) / (act.Jul + 1)) * 100,
+						Aug: ((data.Aug + 1) / (act.Aug + 1)) * 100,
+						Sep: ((data.Sep + 1) / (act.Sep + 1)) * 100,
+						Oct: ((data.Oct + 1) / (act.Oct + 1)) * 100,
+						Nov: ((data.Nov + 1) / (act.Nov + 1)) * 100,
+						Dec: ((data.Dec + 1) / (act.Dec + 1)) * 100,
+						Remarks: &b,	
+					}
+				} else{
+					if data.Jan == 0 {a.Jan = (act.Jan / (data.Jan + 1)) * 100} else {a.Jan = (act.Jan / data.Jan) * 100}
+					if data.Feb == 0 {a.Feb = (act.Feb / (data.Feb + 1)) * 100} else {a.Feb = (act.Feb / data.Feb) * 100}
+					if data.Mar == 0 {a.Mar = (act.Mar / (data.Mar + 1)) * 100} else {a.Mar = (act.Mar / data.Mar) * 100}
+					if data.Apr == 0 {a.Apr = (act.Apr / (data.Apr + 1)) * 100} else {a.Apr = (act.Apr / data.Apr) * 100}
+					if data.May == 0 {a.May = (act.May / (data.May + 1)) * 100} else {a.May = (act.May / data.May) * 100}
+					if data.Jun == 0 {a.Jun = (act.Jun / (data.Jun + 1)) * 100} else {a.Jun = (act.Jun / data.Jun) * 100}
+					if data.Jul == 0 {a.Jul = (act.Jul / (data.Jul + 1)) * 100} else {a.Jul = (act.Jul / data.Jul) * 100}
+					if data.Aug == 0 {a.Aug = (act.Aug / (data.Aug + 1)) * 100} else {a.Aug = (act.Aug / data.Aug) * 100}
+					if data.Sep == 0 {a.Sep = (act.Sep / (data.Sep + 1)) * 100} else {a.Sep = (act.Sep / data.Sep) * 100}
+					if data.Oct == 0 {a.Oct = (act.Oct / (data.Oct + 1)) * 100} else {a.Oct = (act.Oct / data.Oct) * 100}
+					if data.Nov == 0 {a.Nov = (act.Nov / (data.Nov + 1)) * 100} else {a.Nov = (act.Nov / data.Nov) * 100}
+					if data.Dec == 0 {a.Dec = (act.Dec / (data.Dec + 1)) * 100} else {a.Dec = (act.Dec / data.Dec) * 100}
+					a.Remarks = &b
 				}
 				temp = append(temp, a)
 			}
